8b4f8c0f77e0bd4462b861e852ad2e5cf7b08c60
zhuzheng <zhuzheng@startdt.net>
2019-10-31 15:39:35 +0800

    init

---
+++ config/defaultSettings.js
@@ -0,0 +1,34 @@
+export default
+// {
+//   navTheme: 'dark',
+//   primaryColor: '#1890FF',
+//   layout: 'sidemenu',
+//   contentWidth: 'Fluid',
+//   fixedHeader: false,
+//   autoHideHeader: false,
+//   fixSiderbar: false,
+//   colorWeak: false,
+//   menu: {
+//     locale: true,
+//   },
+//   title: 'Ant Design Pro',
+//   pwa: false,
+//   iconfontUrl: '',
+// };
+{
+  "navTheme": "light",
+  "primaryColor": "#13C2C2",
+  "layout": "sidemenu",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "autoHideHeader": false,
+  "fixSiderbar": false,
+  "menu": {
+    "locale": true
+  },
+  "title": "zhuzhengTest",
+  "pwa": false,
+  "iconfontUrl": "",
+  "collapse": true,
+  "language": "zh-CN"
+}
